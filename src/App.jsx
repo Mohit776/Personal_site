beaedd10221d2d4c64e35c3e22cb4104521a2aff
Mohit <mohitaggarwal551@gmail.com>
2025-07-17 16:40:18 +0530

    lenis

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,30 @@ import Skills from "./components/Skills";
 import About from "./components/About";
 import Project from "./components/Project";
 import { Analytics } from '@vercel/analytics/react';
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 function App() {
+   useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      // Cleanup
+      lenis.destroy();
+    };
+  }, []);
+
+
   return (
     <div className="bg-black min-h-screen font-sans">
       <Navbar />
